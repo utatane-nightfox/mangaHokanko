41b02abe494bc82e53d36f6a01934d6001281b95
utatane-nightfox <yorugitune7497@gmail.com>
2025-11-28 11:31:24 +0900

    fix: ログアウトボタンとプロフィール画面の修正

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { supabase } from "@/supabaseClient";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import "@/frameStyles.css";
+import "@/frameStyles";
 
 
 export default function RootLayout({ children }) {
