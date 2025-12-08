a6ba953e646ecd8ad30d35deb4058a606ae0c766
utatane-nightfox <yorugitune7497@gmail.com>
2025-12-08 14:29:09 +0900

    fix: next.ts更新

---
+++ utils/supabaseClient.ts
@@ -0,0 +1,13 @@
+// utils/supabaseClient.ts
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,  // ← ここ必要（callback URL の処理）
+  },
+});
