74a56e61d30fbb60effb97670f3920ff979eb91b
utatane-nightfox <yorugitune7497@gmail.com>
2025-12-09 11:56:13 +0900

    fix: import supabase変更

--- app/auth/callback/AuthCallbackInner.jsx
+++ app/auth/callback/AuthCallbackInner.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { supabase } from "@/utils/supabaseClient";
+import { supabase } from "@/utils/supabaseBrowserClient";
 
 export default function AuthCallbackInner() {
   const router = useRouter();
