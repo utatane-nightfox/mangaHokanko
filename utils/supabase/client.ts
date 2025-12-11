15f0d3cb0951a0ea93075bb8432b58252eacd805
utatane-nightfox <yorugitune7497@gmail.com>
2025-12-11 13:00:15 +0900

    fix: layout.jsxを二分化

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,6 +1,10 @@
+"use client";
+
 import { createBrowserClient } from "@supabase/ssr";
 
-export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+export function supabaseBrowser() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
